631b891bdec530f5bb084c11a1518c8958fbdda2
sjpbailey <sjpbailey1961@gmail.com>
2024-03-23 00:02:03 -0700

    try template Node

--- nodes/PoolController.py
+++ nodes/PoolController.py
@@ -15,7 +15,7 @@ from nodes import Pump2SPDNode
 from nodes import Pump1SPDNode
 from nodes import PumpTriStarNode
 from nodes import PumpHSVSNode
-
+from nodes import TemplateNode
 
 LOGGER = udi_interface.LOGGER
 LOG_HANDLER = udi_interface.LOG_HANDLER
@@ -113,8 +113,11 @@ class PoolController(udi_interface.Node):
                     LOGGER.info(print("Status: {}".format(i["status"])))
                     LOGGER.info("Type External: {}".format(i["type_ext"]))
 
-                    self.poly.addNode(SwitchNode(
-                        self.poly, self.address, address, name, state, status1, self.apiBaseUrl, self.api_url))
+                    self.poly.addNode(TemplateNode(
+                        self.poly, self.address, address, name))  # state, status1, self.apiBaseUrl, self.api_url))
+
+                    # self.poly.addNode(SwitchNode(
+                    #    self.poly, self.address, address, name, state, status1, self.apiBaseUrl, self.api_url))
                     # LOGGER.info('Found {} Circuits'.format(len(self.circuits)))
                     LOGGER.info("Auxillary Installation Complete")
 
